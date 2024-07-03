8ecb70a785626c417c52eee0064ca7a3ec98e854
hetpatel2508 <125752460+hetpatel2508@users.noreply.github.com>
2024-07-03 21:35:15 +0530

    .

---
+++ redux-toolkit-tut/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './Components/Navbar'
+import UserDetails from './Components/UserDetails'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <UserDetails />
+    </>
+  )
+}
+
+export default App
---
+++ redux-toolkit-tut/src/Components/DeleteAllUsers.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import { clearAllUsers } from "../store/slices/UserSlice";
+import {useDispatch} from "react-redux"
+
+function DeleteAllUsers(){
+
+  const dispatch = useDispatch();
+
+  const deleteUsers = () => {
+    dispatch(clearAllUsers())
+  }
+
+  return  <Wrapper>
+  <button className="btn clear-btn" onClick={deleteUsers}>clear users</button>
+  </Wrapper>
+}
+
+const Wrapper = styled.section`
+  .clear-btn{
+    text-transform: capitalize;
+    background-color: #db338a;
+    margin-top:2rem;
+  }
+`
+
+
+export default DeleteAllUsers;
